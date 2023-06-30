4d14ad774ea7efa01442e0d4c78692f924e72590
Gabriel Papke Paganini <gabrielpapkepaganini@192.168.0.10>
2023-06-30 17:02:36 -0300

    feat: theming

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,15 @@
+export const defaultTheme = {
+  white: '#fff',
+  blue: '#3294f8',
+
+  'base-title': '#e7edf4',
+  'base-subtitle': '#C4D4E3',
+  'base-text': '#AFC2D4',
+  'base-span': '#7B96B2',
+  'base-label': '#7C7C8A',
+  'base-border': '#1C2F41',
+  'base-post': '#112131',
+  'base-profile': '#0B1B2B',
+  'base-background': '#071422',
+  'base-input': '#040F1A',
+} as const

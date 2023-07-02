2d6fba6c0450e8117911d8a03e8a99e04e45271c
Gabriel Papke Paganini <gabrielpapkepaganini@192.168.0.4>
2023-07-02 09:14:05 -0300

    feat: aply layoutt and routes

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { HeaderContainer } from './styles'
+
+// import logoIgnite from '../../assets/logo-ignite.svg'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <span>Logo</span>
+    </HeaderContainer>
+  )
+}
